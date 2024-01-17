983e66f5fce4a74df998c98a23d611cc3f724e4e
jayantced <jayant@cedcommerce.com>
2024-01-17 14:30:13 +0530

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ReactNode, useEffect, useState } from "react";
 import { get } from "./util/http";
 import BlogPosts, { BlogPost } from "./components/BlogPosts";
 import fetchingImage from './assets/data-fetching.png'
+import ErrorMessage from "./components/ErrorMessage";
 
 type RawDataBlogPosts = {
   id: number;
@@ -12,22 +13,35 @@ type RawDataBlogPosts = {
 
 function App() {
   const [fetchedposts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [isError, setIsError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts"
-      )) as RawDataBlogPosts[];
 
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
-      
+      setIsFetching(true);
+
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawDataBlogPosts[];
+  
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+
       setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setIsError(error.message)
+        }
+      }
+      
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -35,8 +49,16 @@ function App() {
 
   let content: ReactNode;
 
+  if (isError) {
+    content = <ErrorMessage text={isError} />
+  }
+
   if (fetchedposts) {
     content = <BlogPosts posts={fetchedposts} />
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching Posts...</p>
   }
   return <main>
     <img src={fetchingImage} />
